62c6d948d39aec4956fff4695d6feb44c6d55426
unknown <alan.a.n.r.ar@gmail.com>
2022-12-12 19:44:42 -0300

    new commits

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import './App.css';
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
-import { PrimerComponente } from './components/PrimerComponente';
+
 function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState('')
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=fc0bf223e978b88de9bf74c01a6fb2ce`;
-  
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=fc0bf223e978b88de9bf74c01a6fb2ce&lang=sp`;
+
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
       axios.get(url).then((response) => {
@@ -16,50 +15,54 @@ function App() {
       setLocation('')
     }
   }
-  
+
 
   return (
     <div className="App">
-        <div className='container'>
+      <div className='container'>
         <div className='search'>
-        <input
-          value={location}
-          onChange={event => setLocation(event.target.value)}
-          onKeyPress={searchLocation}
-          placeholder='Enter Location'
-          type='text'
+          <input
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+            onKeyPress={searchLocation}
+            placeholder='Enter Location'
+            type='text'
           />
         </div>
-        <div className='top'>
-          <div className='location'>
-            <p>{data.name}</p>
+        <div className='segundo-container'>
+          <div className={`background-image ${data.weather ? data.weather[0].main : null}`}>
+            <div className={`top ${data.weather ? data.weather[0].main : null}a`}>
+              <div className='location'>
+                <p>{data.name}</p>
+              </div>
+              <div className={`description`}>
+                {data.weather ? <p>{data.weather[0].main}</p> : null}
+              </div>
+            </div>
           </div>
-          <div className='temp'>
-          </div>
-            {data.main ? <h2>{data.main.temp} °F</h2> : null} 
-          <div className='description'>
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
-          </div>
-        </div>
 
-
-        {data.name != undefined && 
-        <div className='bottom'>
-        <div className='feels'>
-         {data.main ? <p>{data.main.feels_like} °F</p> : null}
-         <p>Feels like</p>
+          {data.name != undefined &&
+            <div className='bottom'>
+              <div className='temperature'>
+                <p>Temperature: </p>
+                {data.main ? <h2>{data.main.temp} °C</h2> : null}
+              </div>
+              <div className='feels'>
+                <p>Feels like:</p>
+                {data.main ? <p>{data.main.feels_like} °C</p> : null}
+              </div>
+              <div className='humidity'>
+                <p>Humidity:</p>
+                {data.main ? <p>{data.main.humidity} %</p> : null}
+              </div>
+              <div className='wind'>
+                <p>Wind speed:</p>
+                {data.wind ? <p>{data.wind.speed} MPH</p> : null}
+              </div>
+            </div>
+          }
         </div>
-        <div className='humidity'>
-         {data.main ? <p>{data.main.humidity} %</p> : null}
-         <p>Humidity</p>
-        </div>
-        <div className='wind'>
-        {data.wind ? <p>{data.wind.speed} MPH</p> : null}
-        <p>Wind speed</p>
-        </div>
-        </div>
-        }
-     </div>
+      </div>
     </div>
   );
 }
